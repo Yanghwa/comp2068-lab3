eea0adbe8da270733665f017e4f2aaa2c3d02ab2
Yanghwa <kellerj87@hotmail.com>
2017-01-25 14:30:15 -0500

    lab3 did

--- server.js
+++ server.js
@@ -1,14 +1,42 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+let express = require('express');
+let app = express();
+app.get('/lab3', function(req, res) {
+  let url = req.protocol + '://' + req.get('host') + req.originalUrl;
+  let method = req.param('method');
+  let x =  parseFloat(req.param('x'));
+  let y =  parseFloat(req.param('y'));
+  let result;
+  let output;
+  switch(method) {
+    case('add'):
+      method = '+';
+      result = x+y
+      break;
+    case('subtract'):
+      method = '-';
+      result = x-y
+      break;
+    case('multiply'):
+      method = '*';
+      result = x*y
+      break;
+    case('divide'):
+      method = '/';
+      result = x/y
+      break;
+    default:
+      result = 'You need to put corrent input methods.';
+      break;
+  } 
+  if(isNaN(result)) {
+    output = 'You need to put corrent inputs.' + '\n';
+  } else {
+    output = x +" " + method+ " " +y + " = " + result +'\n'
+  }
+  res.write('Url: ' + url + '\n' + 
+    'Output: ' + output);
+  res.end();
 });
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(3000, function() {
+  console.log('Connected, 3000 port');
 });
